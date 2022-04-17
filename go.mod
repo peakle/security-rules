a3f679110bcb40a01c23f2e1660e02896ebb1398
Denis <d.limarev@corp.mail.ru>
2022-04-17 18:42:42 +0300

    add secure funcs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/peakle/security-rules
+module security-rules
 
 go 1.17
+
+require github.com/quasilyte/go-ruleguard/dsl v0.3.12-0.20220101150716-969a394a9451
--- rules.go
+++ rules.go
@@ -1,1 +1,15 @@
 package rules
+
+import (
+    "github.com/quasilyte/go-ruleguard/dsl"
+)
+
+var Bundle = dsl.Bundle{}
+
+func httpWithoutSSL(m dsl.Matcher)        {}
+func insecureSkipVerify(m dsl.Matcher)    {}
+func oldTLS(m dsl.Matcher)                {}
+func passwordInCode(m dsl.Matcher)        {}
+func oldHashFunc(m dsl.Matcher)           {}
+func oldCryptoAlgorithms(m dsl.Matcher)   {}
+func swaggerBodyValidation(m dsl.Matcher) {}
