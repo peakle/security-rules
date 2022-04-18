d7c9da964928dfedfea94dff6f5428e6496558e9
Denis <d.limarev@corp.mail.ru>
2022-04-18 10:42:38 +0300

    add check by format linters

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ VERSION=$(shell git describe --tags)
 
 test:
 	go test --count=1 -race .
+
+lint:
+	golangci-lint run ./...
+	golangci-lint run ./testdata/src/...
--- rules.go
+++ rules.go
@@ -1,21 +1,21 @@
 package rules
 
 import (
-    "github.com/quasilyte/go-ruleguard/dsl"
+	"github.com/quasilyte/go-ruleguard/dsl"
 )
 
 var Bundle = dsl.Bundle{}
 
 func httpWithoutSSL(m dsl.Matcher) {
-    m.Match(`$_ = $str`,
-        `$_ := $str`,
-        `var $_ $*_= $str`,
-        `const $_ $*_ = $str`,
-        `const $_ = $str`,
-        `$_($*_, $str, $*_)`,
-    ).
-        Where(m["str"].Type.Underlying().Is("string") && m["str"].Const && m["str"].Text.Matches(`http://`)).
-        Report(`HTTP links are not secure`)
+	m.Match(`$_ = $str`,
+		`$_ := $str`,
+		`var $_ $*_= $str`,
+		`const $_ $*_ = $str`,
+		`const $_ = $str`,
+		`$_($*_, $str, $*_)`,
+	).
+		Where(m["str"].Type.Underlying().Is("string") && m["str"].Const && m["str"].Text.Matches(`http://`)).
+		Report(`HTTP links are not secure`)
 }
 
 func insecureSkipVerify(m dsl.Matcher)    {}
